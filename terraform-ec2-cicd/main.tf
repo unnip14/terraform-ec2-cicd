dccde79ac997f2552cf970671784e5d33388e464
root <root@DESKTOP-9K50O4T>
2025-07-07 20:51:35 +0530

    Terraform EC2 CICD

---
+++ terraform-ec2-cicd/main.tf
@@ -0,0 +1,18 @@
+resource "aws_instance" "example" {
+  ami           = var.ami_id
+  instance_type = var.instance_type
+  key_name      = var.key_name
+
+  tags = {
+    Name = "MyTerraformEC2"
+  }
+
+  user_data = <<-EOF
+              #!/bin/bash
+              apt update -y
+              apt install -y httpd
+              systemctl enable httpd
+              systemctl start httpd
+              echo "<h1>Hello from Terraform EC2</h1>" > /var/www/html/index.html
+              EOF
+}
